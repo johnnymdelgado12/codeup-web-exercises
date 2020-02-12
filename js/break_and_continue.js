9de7edb61fedafa1a3d72ea91ba29902bb66a4d4
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-02-12 10:25:56 -0600

    did for loops exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,8 @@
+"use strict"
+
+var userInput = prompt("Enter a number between 1 and 50");
+var numberToSkip = userInput % 2 === 0 || userInput >=50;
+
+
+
+
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,7 +1,48 @@
 "use strict"
 
-function showMultiplicationTable(num) {
-    for (var product = parseFloat(num*=1); ) {
+// function showMultiplicationTable(num) {
+//     for (var i = 1; i <= 10; i++ ) {
+//
+//         var product = num * i;
+//         console.log(num + " x " + i + " = " + product);
+//     }
+// }
+//
+// showMultiplicationTable(5)
 
-    }
-}
+//need to show which numbers are odd
+
+
+
+// for (var i = 0; i > 10; i++);
+// var randomNumber = Math.floor(Math.random() * 200) + 20;
+// if (randomNumber % 2 === 0) {
+//         console.log(randomNumber + " is a even number");
+// } else {
+//         console.log( randomNumber + " That is a odd number");
+//     }
+
+
+
+
+
+    // var rows = 9;
+    // for (var i = 1; i <= rows; i++) {
+    //     var output = '';
+    //     for (var j = 1; j <= i; j++) {
+    //         output  += i;
+    //     }
+    //     console.log(output);
+    // }
+
+
+
+
+// for (var i = 100; i >= 5; i--) {
+    //     if (i % 5 === 0) {
+    //         console.log(i);
+    //     }
+    // }
+
+
+
