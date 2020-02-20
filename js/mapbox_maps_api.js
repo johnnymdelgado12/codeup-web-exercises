a68f97f746b1dc347b1246214ef90d58649a9eb5
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-02-20 16:54:59 -0600

    completed steps 1-7 for exercise

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -6,50 +6,84 @@ var map = new mapboxgl.Map({
     center: [-98.4916, 29.4252]
 });
 
-geocode("119314 N, US-281 #104A, San Antonio, TX 78258", mapboxToken).then(function(results){
+geocode("San Antonio, TX", mapboxToken).then(function(results){
     console.log(results);
     map.setCenter(results);
 });
 
-// var markerOptions = {
-//     zoom: 20,
-//     color: "#f003fc",
-// };
-//
-// // var marker = new mapboxgl.Marker(markerOptions)
-// //     .setLngLat([-98.4616,29.6218])
-// //     .addTo(map);
-//
-// var popupOptions = {
-//     maxWidth: '200px',
-//     closeOnClick: true,
-// };
-//
-// var popup = new mapboxgl.Popup(popupOptions)
-//     .setLngLat([-98.4616,29.6218])
-//     .setHTML("<h1>Ginza Ramen And Poke</h1>")
-//     // .addTo(map);
-//
-// var info = document.createElement('div');
-// info.id = 'marker';
-//
-// var marker = new mapboxgl.Marker(info)
-//     .setLngLat([-98.4616,29.6218])
-//     // .addTo(map)
-//     .setPopup(popup);
-//     popup.addTo(map);
+var popupOptions = {
+    maxWidth: '200px',
+    closeOnClick: true,
+};
 
-// create the popup
-var popup = new mapboxgl.Popup({ offset: 25 }).setText(
-    'Ginza Ramen And Poke'
-);
-
-// create DOM element for the marker
-var info = document.createElement('div');
-info.id = 'marker';
-
-// create the marker
-var marker = new mapboxgl.Marker(info)
-    .setLngLat([-98.4616, 29.6218])
-    .setPopup(popup) // sets a popup on this marker
+var popup = new mapboxgl.Popup(popupOptions)
+    .setLngLat([-98.4616,29.6218])
+    .setHTML("<h1>Ginza Ramen And Poke</h1>")
     .addTo(map);
+
+var markerOptions = {
+    zoom: 20,
+    color: "#f003fc",
+};
+
+var marker = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-98.4616,29.6218])
+    .setPopup(popup)
+    .togglePopup()
+    .addTo(map);
+
+var popup2 = new mapboxgl.Popup(popupOptions)
+    .setLngLat([-98.5001,29.4934])
+    .setHTML("<h1>Ranger Creek Brewery & Distilling</h1>")
+    .addTo(map);
+
+var markerOptions2 = {
+    zoom: 20,
+    color: "#5ae6ce"
+}
+
+var marker2 = new mapboxgl.Marker(markerOptions2)
+    .setLngLat([-98.3970,29.5293])
+    .setPopup(popup2)
+    .togglePopup()
+    .addTo(map);
+
+
+
+var popup3 = new mapboxgl.Popup(popupOptions)
+    .setLngLat([-98.3970,29.5293])
+    .setHTML("<h1>Dog Fathers</h1>")
+    .addTo(map);
+
+var markerOptions3 = {
+    zoom: 20,
+    color: "#f2ff3b"
+}
+
+var marker3 = new mapboxgl.Marker(markerOptions3)
+    .setLngLat([-98.5001,29.4934])
+    .setPopup(popup3)
+    .togglePopup()
+    .addTo(map);
+
+
+var restaurants = [
+    {
+        name: "Ginza Ramen & Poke",
+        address: "19314 N, US-281 #104A, San Antonio, TX 78258",
+        marker: "#f003fc"
+    },
+    {
+        name: "Ranger Creek Brewery & Distilling",
+        address: "4834 Whirlwind Dr, San Antonio, TX 78217",
+        marker: "#5ae6ce"
+    },
+    {
+        name: "Dog Fathers",
+        address: "6211 San Pedro Ave, San Antonio, TX 78216",
+        marker: "#f2ff3b"
+    }
+];
+
+
+
