952a85837ef8ca0e8b435c533c12b691ba4a0315
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-03-08 19:24:26 -0500

    added daily data into cards and added clock

--- js/weather_map.js
+++ js/weather_map.js
@@ -8,16 +8,110 @@ $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/"+ 
 });
 
 function getWeather () {
-    $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/29.4252,-98.4916").done(function (data) {
+    $.ajax("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/" + "/29.4252,-98.4916").done(function (data) {
         console.log(data);
         console.log(new Date(data.currently.time * 1000));
-
-        $.each(data.daily.data, function (i, day) {
-
-        })
-    });
+        $('#weather-right-now').append("Temperature:  " + data.currently.temperature + '<br>' +
+            "Current Weather:  " + data.currently.summary + '<br>' +
+            "Humidity:  " + data.currently.humidity + '<br>' +
+            "Pressure:  " + data.currently.pressure + '<br>' +
+            "Current Time: " + new Date(data.currently.time * 1000));
+// Display the current city by timezone label to the first h2 in body
+        $('.city').append(" " + data.timezone);
+//    Display forecast for today
+        $('#insert-pic-current').attr('src', weatherIcon(data.daily.data[0].icon));
+        $('.today').html(data.daily.data[0].summary + getCurrentDay(data.daily.data[0]) +  '<br>' +
+            "Temperature Low:  " + data.daily.data[0].temperatureLow.toFixed(0) + '&deg' + '<br>' +
+            "Temperature High: " + data.daily.data[0].temperatureHigh.toFixed(0) + '&deg' + '<br>' +
+            "Humidity:  " + data.daily.data[0].humidity * 100 + '%' + '<br>' +
+            "Pressure:  " + (data.daily.data[0].pressure / 33.864).toFixed(2) + ' inHg' + '</p>');
+// Display forecast for tomorrow
+        $('#insert-pic-next').attr('src', weatherIcon(data.daily.data[1].icon));
+        $('.tomorrow').append(data.daily.data[1].summary + '<br>' +
+            "Temperature Low:  " + data.daily.data[1].temperatureLow.toFixed(0) +  '&deg' + '<br>' +
+            "Temperature High: " + data.daily.data[1].temperatureHigh.toFixed(0) + '&deg' + '<br>' +
+            "Humidity:  " + data.daily.data[1].humidity * 100 + '%' + '<br>' +
+            "Pressure:  " + (data.daily.data[1].pressure / 33.864).toFixed(2) + ' inHg' + '</p>');
+// Display forecast for day after tomorrow
+        $('#insert-pic-next-after').attr('src', weatherIcon(data.daily.data[2].icon));
+        $('.next-day').append(data.daily.data[2].summary + '<br>' +
+            "Temperature Low:  " + data.daily.data[2].temperatureLow.toFixed(0) + '&deg' + '<br>' +
+            "Temperature High: " + data.daily.data[2].temperatureHigh.toFixed(0) + '&deg' + '<br>' +
+            "Humidity:  " + data.daily.data[2].humidity * 100 + '%' + '<br>' +
+            "Pressure:  " + (data.daily.data[2].pressure  / 33.864).toFixed(2) + ' inHg' + '</p>');
+    })
 }
 getWeather();
+
+function weatherIcon(condition) {
+    var icon = '';
+    weatherIcons.forEach(function (i) {
+        if (i.condition === condition){
+            icon = i.icon;
+        }
+    });
+    return icon
+}
+var d = new Date()
+
+function getCurrentDay(days) {
+    var dow = '';
+    switch (days) {
+        case 0:
+          dow =  "Sunday";
+            break;
+        case 1:
+           dow = "Monday";
+            break;
+        case 2:
+           dow = "Tuesday";
+            break;
+        case 3:
+           dow =  "Wednesday";
+            break;
+        case 4:
+           dow = "Thursday";
+            break;
+        case 5:
+           dow = "Friday";
+            break;
+        case 6:
+           dow = "Saturday"
+    }
+    return dow;
+}
+
+console.log(getCurrentDay());
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+
+showTime();
 
 mapboxgl.accessToken = mapboxToken;
 var map = new mapboxgl.Map({
@@ -37,7 +131,8 @@ var geoCoder = new MapboxGeocoder({
     mapboxgl: mapboxgl,
     marker: false
 }).on('result', function (data) {
-    marker.setLngLat(data.result.center)
+    marker.setLngLat(data.result.center);
+    console.log(data);
 });
 
 map.addControl(geoCoder);
@@ -49,16 +144,18 @@ var markerOptions = {
 
 var marker = new mapboxgl.Marker(markerOptions)
     .setLngLat([-98.4916, 29.4252])
-    .addTo(map).on('dragend', getCurrentLocation());
+    .addTo(map).on('dragend', getCurrentLocation);
 
 
 function getCurrentLocation() {
-    geoCoder.on('result', function (data) {
+    $.ajax('https://api.mapbox.com/geocoding/v5/mapbox.places/' + marker.getLngLat().lng + ',' + marker.getLngLat().lat + '.json?access_token=' + mapboxToken).done(function(data) {
         console.log(data);
     });
 }
 
-var  weather = [
+
+
+var  weatherIcons = [
     {
         condition: "clear-day",
         icon: "icons/clear-day.png"
@@ -101,3 +198,12 @@ var  weather = [
     }
 ];
 
+var dayOfTheWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
