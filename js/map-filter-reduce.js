b90cb16dbdcd71c676b3d5d20c409a6785eab0de
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-03-11 09:23:03 -0500

    updated exercises for map, filter, reduce

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -51,10 +51,34 @@ console.log(languagesKnown);
 
 //     // 3. Use .map to create an array of strings where each element is a user's email address
 
+let emails = users.map(user => user.email);
 
+console.log(emails);
 
-//     // 4. Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+//     // 4. Use .reduce to get the total years of experience from the list of users.
+//     Once you get the total of years you can use the result to calculate the average.
+
+let totalXP = users.reduce((total, user) => total + user.yearsOfExperience, 0);
+console.log(totalXP);
+console.log((totalXP / users.length));
+
 //     // 5. Use .reduce to get the longest email from the list of users.
-//     // 6. Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+//my solution
+let longest = users.reduce((userA, userB) => userA.email.length > userB.email.length ? userA : userB);
+
+console.log(longest.email);
+//instructor solution
+let longestEmail = users.reduce((currentLongest, user) => user.email.length > currentLongest.length ? user.email :
+   currentLongest , 'i');
+
+console.log(longestEmail);
+
+//     // 6. Use .reduce to get the list of user's names in a single string.
+//     Example: Your instructors are: ryan, luis, zach, fernando, justin.
+
+let stringOfNames = users.reduce((sentence, user) => sentence === '' ? user.name : sentence + ', ' + user.name,'');
+
+console.log('Your instructors are: ' + stringOfNames);
+
 
 
