26f1823385343110fcee836667ba68c7190a3b3c
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-03-11 15:39:30 -0500

    completed second half of promises exercise

--- js/promises.js
+++ js/promises.js
@@ -17,16 +17,29 @@
 //     })
 // }
 
-const wait = num => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, num)
+// const wait = num => {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, num)
+//     })
+// };
+
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+//
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+
+
+fetch('https://api.github.com/users/johnnymdelgado12/events' , {headers: {'Authorization': gitHubToken}})
+    .then(response=>{
+        // console.log(response);
+        return response.json();
     })
-};
+    .then(jsonResponse=>{
+        // console.log(jsonResponse);
+        return console.log(new Date(jsonResponse[0].created_at));
+    });
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
-fetch(url, {headers: {'Authorization': gitHubToken}});
 
